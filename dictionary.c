e04f1a2437265e36a1363d42923bbc4df20a7f3f
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-05-13 12:07:20 -0400

    PointDictionary Debugged and Tested (IT WORKS :D)

--- dictionary.c
+++ dictionary.c
@@ -30,8 +30,8 @@ https://stackoverflow.com/questions/4384359/quick-way-to-implement-dictionary-in
 */
 typedef struct PointDictionary {
     struct PointDictionary *next;
-    const char* key;
-    Point value;
+    const char *key;
+    Point *value;
 } PointDictionary;
 
 // Create a table of hash values
@@ -56,31 +56,66 @@ PointDictionary *find(char *key) {
 
 Point find_value(char *key) {
     PointDictionary *np = find(key);
-    return np->value;
+    Point *val = np->value;
+    return *val;
 }
 
-PointDictionary *install(char *key, Point value) {
+PointDictionary *install(char *key, Point *value) {
     PointDictionary *np;
     unsigned hashvalue;
     if ((np = find(key)) == NULL) { // Set np as the instance at key
         np = (PointDictionary *)malloc(sizeof(*np));
+        np->value = value;
         if (np == NULL || (np->key = strdup(key)) == NULL) return NULL;
         hashvalue = hash(key); // Get next hash in table
         np->next = table[hashvalue]; // Set next link to the new hash according to the table
         table[hashvalue] = np;
     } else {
-        free((void *) np->value); // Free up previous value assignment
+        free((Point *) np->value); // Free up previous value assignment
         np->value = value;
         return np;
     }
 }
 
+PointDictionary *generate_directions() {
+    PointDictionary *dict = (PointDictionary *)malloc(sizeof(PointDictionary)*8);
+
+    Point *nw = (Point *)malloc(sizeof(Point));
+    nw->h_point = 2;
+    nw->v_point = 0;
+    Point *n = (Point *)malloc(sizeof(Point));
+    n->h_point = 1;
+    n->v_point = 0;
+    Point *ne = (Point *)malloc(sizeof(Point));
+    ne->h_point = 0;
+    ne->v_point = 0;
+    Point *e = (Point *)malloc(sizeof(Point));
+    e->h_point = 0;
+    e->v_point = 1;
+    Point *se = (Point *)malloc(sizeof(Point));
+    se->h_point = 0;
+    se->v_point = 2;
+    Point *s = (Point *)malloc(sizeof(Point));
+    s->h_point = 1;
+    s->v_point = 2;
+    Point *sw = (Point *)malloc(sizeof(Point));
+    sw->h_point = 2;
+    sw->v_point = 2;
+    Point *w = (Point *)malloc(sizeof(Point));
+    w->h_point = 2;
+    w->v_point = 1;
+
+    char* keys[8] = {"NW","N","NE","E","SE","S","SW","W"};
+    Point* points[8] = {nw,n,ne,e,se,s,sw,w};
+
+    for (int i=0;i<8;i++) {
+        dict = install(keys[i],points[i]);
+    }
+}
+
 int main() {
-    PointDictionary *dummydict;
-    Point nw;
-    nw.h_point = 0;
-    nw.v_point = -1;
-    dummydict = install("NW",nw);
-    Point test = find_value("NW");
+    PointDictionary *point_dict = generate_directions();
+
+    Point test = find_value("W");
     printf("%d %d\n",test.h_point,test.v_point);
 }
